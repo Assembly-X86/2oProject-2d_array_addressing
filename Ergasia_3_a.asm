23aa740b4ddebda4d6f22fdc6137a3b069ddec96
dit17178 <dit17178@office365.uop.gr>
2021-03-15 15:22:17 +0200

    Δημιουργεια μυνηματων

--- Ergasia_3_a.asm
+++ Ergasia_3_a.asm
@@ -2,8 +2,12 @@ title 2d_array_addressing
 INCLUDE Irvine32.inc
 
 .data
-	
-	array DWORD
+	;τα μυνηματα που θα εμφανιζονται
+
+	mCalculating BYTE "Calculating...",0dh,0ah,0
+	mAccessElement byte "The value of the element a[2,2] is :",0
+	mResultSum BYTE "The sum a[2,2]+z= :",0
+	mResultDiff BYTE "The sum a[2,2]-z= :",0
 	
 
 .code
